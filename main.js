ee041cbf398b7af5c30d439b6786e3b2650450c3
Devv-Abdullah <159600028+Devv-Abdullah@users.noreply.github.com>
2024-10-04 17:04:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+function phones(phone){
+    img.src = phone;
+}
+
+let containar = document.querySelector('.containar');
+function colors(color){
+    containar.style.background = color
+}
